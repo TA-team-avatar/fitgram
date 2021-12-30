a2609c7b3916689972eab63bda826a23ae5bff19
jakedoublev <vanvorhisjake@gmail.com>
2021-12-29 19:56:50 -0800

    fixed typo

--- client/LoginSignupPage.js
+++ client/LoginSignupPage.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-const lLoginSignupPage = (props) => {
+const LoginSignupPage = (props) => {
   return (
     <div>
       <h1 className="app-name">Follow Fit</h1>
-      <h3 className="tagline"></h3>
-      {/* //login fields component here //OAuth google sign in here //then signup
-      button and opening component here */}
+      <h3 className="tagline">marketing copy placeholder</h3>
+      {/* //login fields component here 
+      //OAuth google sign in here 
+      //then signup button and opening component here */}
       <img
         src="https://images.pexels.com/photos/3601094/pexels-photo-3601094.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
         className="login-page-background-image"
@@ -16,4 +17,4 @@ const lLoginSignupPage = (props) => {
   );
 };
 
-export default lLoginSignupPage;
+export default LoginSignupPage;
