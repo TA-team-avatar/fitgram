d16f13110db371c6069c4484495a971bc95d3f89
jakedoublev <vanvorhisjake@gmail.com>
2021-12-29 19:51:22 -0800

    finished login page container ready for other components to be added later

---
+++ client/LoginSignupPage.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+const lLoginSignupPage = (props) => {
+  return (
+    <div>
+      <h1 className="app-name">Follow Fit</h1>
+      <h3 className="tagline"></h3>
+      {/* //login fields component here //OAuth google sign in here //then signup
+      button and opening component here */}
+      <img
+        src="https://images.pexels.com/photos/3601094/pexels-photo-3601094.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        className="login-page-background-image"
+        style={{ width: "100vw" }}
+      ></img>
+    </div>
+  );
+};
+
+export default lLoginSignupPage;
