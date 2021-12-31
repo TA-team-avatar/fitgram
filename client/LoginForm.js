7933c9693f2107bfa8463b168af4cb783f1a342f
scott <sktatsuno@gmail.com>
2021-12-30 17:49:50 -0800

    login form created

--- client/LoginForm.js
+++ client/LoginForm.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const LoginForm = () => {
+const LoginForm = (props) => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(username);
+    console.log(password);
+  }
 
 	return (
 		<div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form>
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" onChange={e => setUsername(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input type="password" onChange={e => setPassword(e.target.value)}/>
         </label>
         <div>
           <button type="submit">Login</button>
--- client/LoginSignupPage.js
+++ client/LoginSignupPage.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
+import LoginForm from "./LoginForm";
 
 const LoginSignupPage = (props) => {
   return (
     <div>
       <h1 className="app-name">Follow Fit</h1>
       <h3 className="tagline">marketing copy placeholder</h3>
-      {/* //login fields component here 
-      //OAuth google sign in here 
+      <LoginForm />
+      {/* //OAuth google sign in here 
       //then signup button and opening component here */}
       <img
         src="https://images.pexels.com/photos/3601094/pexels-photo-3601094.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
