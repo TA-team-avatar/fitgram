63ecc3474c0bae0e43fdea59efea00fe24b54bb9
jakedoublev <vanvorhisjake@gmail.com>
2021-12-30 18:55:09 -0800

    working on frontend google auth still

--- client/GoogleLogin.js
+++ client/GoogleLogin.js
@@ -3,13 +3,22 @@ import { GoogleLogin } from "react-google-login";
 
 export default function GoogleLogin() {
   const clientId = process.env.CLIENT_ID;
+
+  const onLoginFailure = (res) => {
+    console.log("Login Successful:", res.profileObj);
+  };
+
+  const onLoginSuccess = (res) => {
+    console.log("Login failed:", res);
+  };
+
   return (
     <div>
       <GoogleLogin
         clientId={clientId}
         buttonText="Login with Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
+        onSuccess={onLoginSuccess}
+        onFailure={onLoginFailure}
         cookiePolicy={"single_host_origin"}
       />
     </div>
