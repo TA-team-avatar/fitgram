8c280d320fddd90221dd4a79a3a09a54625e76c8
Vastille Dolcine <vdolcine@gmail.com>
2022-01-03 18:36:47 -0800

    Added the dashboard container

---
+++ client/DashboardContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const DashboardContainer = (props) => {
+  return (
+    <div className='dashboard-container'>
+    </div>
+  );
+};
+
+export default DashboardContainer;
