a025777be74de4fe9a4d0968a1d9f5dd275715b1
scott <sktatsuno@gmail.com>
2022-01-03 19:36:35 -0800

    feed and card components created

--- client/Feed.js
+++ client/Feed.js
@@ -2,28 +2,48 @@ import React, { useState, useEffect } from "react";
 import WorkoutCard from "./WorkoutCard";
 
 const Feed = (props) => {
-	const [workoutList, setWorkoutList] = useState([]);
+	const [workoutsList, setWorkoutsList] = useState([]);
 
 	// on mount fetch workout-list from server
 	useEffect(() => {
-    fetch('/workoutsList')
-		  .then(response)
-		// set state
-    
+    fetch('/workouts-list')
+			.then(res => res.json())
+			// set state
+			.then(data => setWorkoutsList(data.workoutsList));
 	}, [])
 
 	const workoutCards = []
-	workoutList.forEach((workout, i) => {
-		workoutCards.push(<WorkoutCard workout={workout} key={i}></WorkoutCard>);
+	console.log(Array.isArray(workoutsList))
+	console.log(workoutsList)
+	workoutsList.forEach((workout, i) => {
+		workoutCards.push(<WorkoutCard 
+			workoutContent={workout['workout_content']}
+			date={workout['date']}
+			athleteId={workout['athlete_id']}
+			key={workout['_id']}
+			>
+			</WorkoutCard>);
 	});
 
 	return(
-		<div id="feed">
+		<div id="feed" style={styles.container}>
 			{workoutCards}
 		</div>
 	)
 
 
 };
+
+const styles = {
+  container: {
+    border: '1px black solid',
+    width: '50%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '10px',
+  },
+};
+
 
 export default Feed;
--- client/WorkoutCard.js
+++ client/WorkoutCard.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
 
-const WorkoutCard = ({workout}) => {
+const WorkoutCard = ({workoutContent, date, athleteId}) => {
 	return (
-    <div>
-			{workout}
+    <div style={styles.container}>
+			<div>{workoutContent}</div>
+			<div>Athlete Name: {athleteId} Date: {date}</div>
 		</div>
 	);
+};
+
+const styles = {
+  container: {
+    border: '1px solid black',
+    height: 100,
+    width: '100%',
+    flex: 1,
+  },
 };
 
 export default WorkoutCard;
