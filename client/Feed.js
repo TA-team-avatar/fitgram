d2536efc91fc097ca41600ae6ff4a917384bc554
scott <sktatsuno@gmail.com>
2022-01-04 19:57:00 -0800

    resolved merge conflict with dev pull

--- client/Feed.js
+++ client/Feed.js
@@ -2,35 +2,6 @@ import React, { useState, useEffect } from "react";
 import WorkoutCard from "./WorkoutCard";
 
 const Feed = (props) => {
-<<<<<<< HEAD
-  const [workoutsList, setWorkoutsList] = useState([]);
-
-  // on mount fetch workout-list from server
-  useEffect(() => {
-    fetch("/workouts-list")
-      .then((res) => res.json())
-      // set state
-      .then((data) => setWorkoutsList(data.workoutsList));
-  }, []);
-
-  const workoutCards = [];
-  workoutsList.forEach((workout, i) => {
-    workoutCards.push(
-      <WorkoutCard
-        workoutContent={workout["workout_content"]}
-        date={workout["date"]}
-        athleteId={workout["athlete_id"]}
-        key={workout["_id"]}
-      ></WorkoutCard>
-    );
-  });
-
-  return (
-    <div id="feed" style={styles.container}>
-      {workoutCards}
-    </div>
-  );
-=======
 	const [workoutsList, setWorkoutsList] = useState([]);
 
 	// on mount fetch workout-list from server
@@ -61,7 +32,6 @@ const Feed = (props) => {
 	)
 
 
->>>>>>> dev
 };
 
 const styles = {
