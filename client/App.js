dae3867822db73bc56c6c8c843e66774cd2d5231
Trang <felisityng16@gmail.com>
2022-01-04 17:54:51 -0800

    working on react-router

--- client/App.js
+++ client/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import LoginSignupPage from "./LoginSignupPage";
 
 ReactDOM.render(
-  <LoginSignupPage></LoginSignupPage>,
+  <Router>
+    <Routes>
+      <LoginSignupPage></LoginSignupPage>
+    </Routes>
+  </Router>,
   document.getElementById("root")
 );
