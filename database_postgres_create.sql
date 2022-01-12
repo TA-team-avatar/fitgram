ea714ace963e5c06d590d833c50fdd9af98356f6
James Kim <james.minjae.97@gmail.com>
2022-01-12 00:12:08 -0500

    connected to database

--- database_postgres_create.sql
+++ database_postgres_create.sql
@@ -1,18 +1,129 @@
-
-
-CREATE TABLE IF NOT EXISTS athletes (
-    "_id" serial NOT NULL, 
-    "email_address" varchar NOT NULL, 
-    "password" varchar,
-    "athlete_name" varchar NOT NULL,
-    "age" int,
-    CONSTRAINT "athletes_id_pk" PRIMARY KEY ("_id") 
+CREATE TABLE "public.users" (
+	"id" serial NOT NULL,
+	"user_name" TEXT NOT NULL,
+	"first_name" TEXT NOT NULL,
+	"last_name" TEXT NOT NULL,
+	"email" TEXT NOT NULL,
+	"height" float8 NOT NULL,
+	"weight" float8 NOT NULL,
+	"password" TEXT NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
 
-CREATE TABLE IF NOT EXISTS workout_card (
-    "_id" serial NOT NULL, 
-    "workout_content" char NOT NULL, 
-    "date" timestamp,
-    "athlete_id" serial REFERENCES athletes("_id"),
-    CONSTRAINT "workout_card_id_pk" PRIMARY KEY ("_id")
+
+
+CREATE TABLE "public.workouts" (
+	"id" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	"description" TEXT NOT NULL,
+	CONSTRAINT "workouts_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "public.routines" (
+	"id" serial NOT NULL,
+	"owner_user_id" bigint NOT NULL,
+	"name" TEXT NOT NULL,
+	"duration" bigint NOT NULL,
+	CONSTRAINT "routines_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.routine_workout" (
+	"id" serial NOT NULL,
+	"routine_id" bigint NOT NULL,
+	"workout_id" bigint NOT NULL,
+	"set" bigint NOT NULL,
+	"repetition_motion" bigint,
+	"day" bigint NOT NULL,
+	CONSTRAINT "routine_workout_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.forums" (
+	"id" serial NOT NULL,
+	"owner_user_id" int8 NOT NULL,
+	"routine_id" int8 NOT NULL,
+	"name" TEXT NOT NULL,
+	"likes" int8 NOT NULL,
+	"dislikes" int8 NOT NULL,
+	"date_created" TIMESTAMP NOT NULL,
+	CONSTRAINT "forums_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.comments" (
+	"id" serial NOT NULL,
+	"owner_user_id" bigint NOT NULL,
+	"forum_id" bigint NOT NULL,
+	"description" TEXT NOT NULL,
+	"date_created" TIMESTAMP NOT NULL,
+	CONSTRAINT "comments_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.messages" (
+	"id" serial,
+	"sender_id" bigint NOT NULL,
+	"recepient_id" bigint NOT NULL,
+	"description" TEXT NOT NULL,
+	"date_created" TIMESTAMP NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.sessions" (
+	"id" serial NOT NULL,
+	"token" TEXT NOT NULL,
+	"user_id" int8 NOT NULL,
+	CONSTRAINT "sessions_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "routines" ADD CONSTRAINT "routines_fk0" FOREIGN KEY ("owner_user_id") REFERENCES "users"("id");
+
+ALTER TABLE "routine_workout" ADD CONSTRAINT "routine_workout_fk0" FOREIGN KEY ("routine_id") REFERENCES "routines"("id");
+ALTER TABLE "routine_workout" ADD CONSTRAINT "routine_workout_fk1" FOREIGN KEY ("workout_id") REFERENCES "workouts"("id");
+
+ALTER TABLE "forums" ADD CONSTRAINT "forums_fk0" FOREIGN KEY ("owner_user_id") REFERENCES "users"("id");
+ALTER TABLE "forums" ADD CONSTRAINT "forums_fk1" FOREIGN KEY ("routine_id") REFERENCES "routines"("id");
+
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk0" FOREIGN KEY ("owner_user_id") REFERENCES "users"("id");
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk1" FOREIGN KEY ("forum_id") REFERENCES "forums"("id");
+
+ALTER TABLE "messages" ADD CONSTRAINT "messages_fk0" FOREIGN KEY ("sender_id") REFERENCES "users"("id");
+ALTER TABLE "messages" ADD CONSTRAINT "messages_fk1" FOREIGN KEY ("recepient_id") REFERENCES "users"("id");
+
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+
+
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const path = require("path");
-const bodyParser = require("body-parser");
-const env = require("dotenv").config();
-const PORT = process.env.PORT;
-const queriesRouter = require("./queriesRoutedToDB");
-const authentification = require("./authentification");
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const env = require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const queriesRouter = require('./queriesRoutedToDB');
+const authentification = require('./authentification');
 
 app.use(cors());
 
@@ -15,50 +15,58 @@ app.use(cors());
  */
 app.use(express.json());
 
+// app.get('/db-test', async (req, res) => {
+//   const result = await queriesRouter.query(
+//     'SELECT id, user_name, first_name, last_name, email, height, weight, password FROM public."public.users";'
+//   );
+//   console.log(result.rows);
+//   return res.status(200);
+// });
+
 //handle workouts-list query for workout cards data
-app.get("/workouts-list", queriesRouter.getWorkoutsList, (req, res) => {
+app.get('/workouts-list', queriesRouter.getWorkoutsList, (req, res) => {
   const { workoutsList } = res.locals;
   return res.status(200).json({ workoutsList });
 });
 
 //handle athlete-workouts list query to get all the workouts for one athlete
-app.get("/athlete-workouts", queriesRouter.getWorkoutsByAthlete, (req, res) => {
+app.get('/athlete-workouts', queriesRouter.getWorkoutsByAthlete, (req, res) => {
   const { workoutsList } = res.locals;
   return res.status(200).json({ workoutsList });
 });
 
 //handle request to get the name of the athlete by the Id
-app.get("/athlete-info", queriesRouter.getAthleteInfo, (req, res) => {
+app.get('/athlete-info', queriesRouter.getAthleteInfo, (req, res) => {
   const { athleteName } = res.locals;
   return res.status(200).json({ athleteName });
 });
 
 //handle post-workout route to add a workout to workout_card table
-app.post("/post-workout", queriesRouter.postWorkout, (req, res) => {
-  return res.status(200).send("workout posted");
+app.post('/post-workout', queriesRouter.postWorkout, (req, res) => {
+  return res.status(200).send('workout posted');
 });
 
 app.post(
-  "/api/google-auth",
+  '/api/google-auth',
   authentification.googleVerify,
   authentification.getAthleteId,
   (req, res) => {
     //gets the userId from the middleware's res.locals (once authenticated and/or created in table)
     //then sends it as a cookie to the front-end so state can grab it where needed
     const { athlete_id } = res.locals;
-    res.cookie("athleteId", athlete_id, {
+    res.cookie('athleteId', athlete_id, {
       expires: new Date(Date.now() + 1800000),
     });
-    return res.status(201).send("cookie sent");
+    return res.status(201).send('cookie sent');
   }
 );
 
 //global error middleware
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: "Express error handler caught unknown middleware error",
+    log: 'Express error handler caught unknown middleware error',
     status: 500,
-    message: { err: "An error occurred" },
+    message: { err: 'An error occurred' },
   };
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
