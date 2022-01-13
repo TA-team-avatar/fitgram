3020fe77e7f154508a2ce5c1cbf052eebca420b1
James Kim <james.minjae.97@gmail.com>
2022-01-13 16:08:55 -0500

    added delete routine and responding with routines with request in getroutines

--- server/controller/routinesController.js
+++ server/controller/routinesController.js
@@ -17,12 +17,57 @@ routinesController.getRoutines = async (req, res, next) => {
   const paramRoutine = [id];
   try {
     const routines = await db.query(queryRoutine, paramRoutine);
-    console.log(routines.rows);
+    res.locals.routines = routines.rows;
 
     return next();
   } catch (err) {
     return next({
-      log: `Error with routinesController.getRoutines Error: ${err}`,
+      log: `Error with routinesController.getRoutines ${err}`,
+      message: { err: `error from routinesController ${err}` },
+    });
+  }
+};
+
+routinesController.insertRoutine = async (req, res, next) => {
+  const { user_id, name, duration, routine_workout } = req.body;
+
+  const queryRoutine =
+    'INSERT INTO routines (owner_user_id, name, duration)\
+  VALUES ($1, $2, $3)\
+  RETURNING id';
+  const paramRoutine = [user_id, name, duration];
+
+  const queryRoutineWorkout =
+    'INSERT INTO routine_workout (routine_id, workout_id, set, repetition_motion, day)\
+  VALUES ($1, $2, $3, $4, $5)\
+  RETURNING *';
+
+  try {
+    const insertRoutine = await db.query(queryRoutine, paramRoutine);
+    console.log('routine -->', insertRoutine.rows);
+
+    await Promise.all(
+      routine_workout.map(async (rw) => {
+        const params = [];
+        params[0] = insertRoutine.rows[0].id;
+        params[1] = rw.workout_id;
+        params[2] = rw.set;
+        params[3] = rw.repetition_motion;
+        params[4] = rw.day;
+
+        const insertRoutineWorkout = await db.query(
+          queryRoutineWorkout,
+          params
+        );
+        console.log(insertRoutineWorkout.rows);
+        return;
+      })
+    );
+
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error with routinesController.insertRoutine ${err}`,
       message: { err: `error from routinesController ${err}` },
     });
   }
@@ -37,15 +82,13 @@ routinesController.deleteRoutine = async (req, res, next) => {
     'DELETE FROM routine_workout WHERE routine_id = $1';
   const paramRoutineWorkout = [id];
   try {
-    const deleteRoutine = await db.query(queryRoutine, paramRoutine);
-    const deleteRoutineWorkout = await db.query(
-      queryRoutineWorkout,
-      paramRoutineWorkout
-    );
+    await db.query(queryRoutine, paramRoutine);
+    await db.query(queryRoutineWorkout, paramRoutineWorkout);
+
     return next();
   } catch (err) {
     return next({
-      log: `Error with routinesController.deleteRoutine Error: ${err}`,
+      log: `Error with routinesController.deleteRoutine ${err}`,
       message: { err: `error from routinesController ${err}` },
     });
   }
--- server/routes/routines.js
+++ server/routes/routines.js
@@ -3,14 +3,18 @@ const routineController = require('./../controller/routinesController.js');
 
 const router = express.Router();
 
-const { getRoutines, deleteRoutine } = routineController;
+const { getRoutines, insertRoutine, deleteRoutine } = routineController;
 
 router.get('/', getRoutines, (req, res) => {
+  return res.status(200).json({ routines: res.locals.routines });
+});
+
+router.post('/', insertRoutine, (req, res) => {
   return res.sendStatus(200);
 });
 
 router.delete('/', deleteRoutine, (req, res) => {
-  return res.sendStatus(200);
+  return res.sendStatus(204);
 });
 
 module.exports = router;
