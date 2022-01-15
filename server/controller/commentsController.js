6a542a37ccb61e894f0b0d9c5c9076970229a8bc
Mia Kang <miakangnyc@gmail.com>
2022-01-15 16:36:25 -0500

    working on getComments middleware still, merged from dev

--- server/controller/commentsController.js
+++ server/controller/commentsController.js
@@ -4,6 +4,17 @@ const commentsController = {};
 
 commentsController.getComments = (req, res, next) => {
   console.log('reached getComments');
+  const getCommentsQuery = `
+  SELECT * FROM comments
+  ORDER BY date_created
+  `;
+
+  try {
+      return next();
+  }
+  catch (err) {
+      return next({'error log'});
+  }
 
   return next();
 };
