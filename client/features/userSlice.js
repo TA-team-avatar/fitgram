d6916e9bf0e7af2d82e42ac150e1f0604a7cd8c4
James Kim <james.minjae.97@gmail.com>
2022-01-18 23:40:01 -0500

    added back reducers

--- client/features/userSlice.js
+++ client/features/userSlice.js
@@ -44,7 +44,30 @@ export const getUserName = createAsyncThunk(
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    // getUserId: (state, action) => {
+    //   const token = action.payload.token;
+    //   /**
+    //    * TODO: Make API call to get User ID from the server.
+    //    */
+    //   const res = dummyData.user;
+    //   state.userId = res;
+    // },
+    // getUserName: (state, action) => {
+    //   const userId = action.payload.userId;
+    //   // console.log('line 25', userId);
+    //   /**
+    //    * TODO: Make API call to get User Name from the server.
+    //    */
+    //   let res = dummyData.users.filter((user) => user.id === userId)[0];
+    //   if (res) {
+    //     res = JSON.parse(JSON.stringify(res));
+    //   }
+    //   // console.log('line 29', res);
+    //   state.userData = res;
+    //   // console.log('state.userData', state.userData);
+    // },
+  },
   extraReducers: {
     [loginUser.pending]: (state) => {
       state.status = 'loginUser api is pending';
