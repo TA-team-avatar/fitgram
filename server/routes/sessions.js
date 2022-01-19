342a1e0e4191c765e64c9c7cbb59f158c04f827d
James Kim <88161114+Jamesmjkim@users.noreply.github.com>
2022-01-18 23:49:16 -0500

    Update sessions.js

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -7,7 +7,7 @@ const { checkSession, addSession } = sessionsController;
 
 router.post('/token', checkSession, (req, res) => {
   console.log('exiting /sessions CHECKING SESSION');
-  console.log(res.locals);
+//   console.log(res.locals);
   return res
     .status(200)
     .json({ user_id: res.locals.user_id, token: res.locals.token });
