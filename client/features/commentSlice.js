a9d9a4f7f0c96a7a7e49b9811e9bfab90a4067ab
Mia Kang <miakangnyc@gmail.com>
2022-01-19 21:19:52 -0500

    pupdated dev

--- client/features/commentSlice.js
+++ client/features/commentSlice.js
@@ -3,75 +3,76 @@ import dummyData from '../constants/dummyData';
 
 const initialState = {
   commentData: [],
+  status: null;
 };
 
 export const commentSlice = createSlice({
   name: 'comment',
   initialState,
   reducers: {
-    getForumComments: (state, action) => {
-      const forumId = action.payload.forumId;
+    // getForumComments: (state, action) => {
+    //   const forumId = action.payload.forumId;
 
-      /**
-       * TODO: Make API call to get forum comment information
-       */
-      let res = dummyData.comments.filter(
-        (comment) => comment.forum_id === forumId
-      );
+    //   /**
+    //    * TODO: Make API call to get forum comment information
+    //    */
+    //   let res = dummyData.comments.filter(
+    //     (comment) => comment.forum_id === forumId
+    //   );
 
-      if (res) {
-        res = JSON.parse(JSON.stringify(res));
-      }
+    //   if (res) {
+    //     res = JSON.parse(JSON.stringify(res));
+    //   }
 
-      state.commentData = res;
-    },
-    createComments: (state, action) => {
-      const { description, owner_user_id, forum_id } = action.payload;
-      /**
-       * TODO: Make API call to add comments to db
-       */
+    //   state.commentData = res;
+    // },
+    // createComments: (state, action) => {
+    //   const { description, owner_user_id, forum_id } = action.payload;
+    //   /**
+    //    * TODO: Make API call to add comments to db
+    //    */
 
-      const comments = JSON.parse(JSON.stringify(dummyData.comments));
-      comments.push({
-        id: 4,
-        description,
-        owner_user_id,
-        forum_id,
-        date_created: '2022-01-15',
-        user_name: 'Han',
-      });
-      state.commentData = comments;
-    },
-    deleteComments: (state, action) => {
-      const { id } = action.payload;
-      /**
-       * TODO: Make API call to delete comments to db
-       */
+    //   const comments = JSON.parse(JSON.stringify(dummyData.comments));
+    //   comments.push({
+    //     id: 4,
+    //     description,
+    //     owner_user_id,
+    //     forum_id,
+    //     date_created: '2022-01-15',
+    //     user_name: 'Han',
+    //   });
+    //   state.commentData = comments;
+    // },
+    // deleteComments: (state, action) => {
+    //   const { id } = action.payload;
+    //   /**
+    //    * TODO: Make API call to delete comments to db
+    //    */
 
-      let res = dummyData.comments.filter((comment) => comment.id !== id);
+    //   let res = dummyData.comments.filter((comment) => comment.id !== id);
 
-      if (res) {
-        res = JSON.parse(JSON.stringify(res));
-      }
+    //   if (res) {
+    //     res = JSON.parse(JSON.stringify(res));
+    //   }
 
-      state.commentData = res;
-    },
-    editComments: (state, action) => {
-      const { id, description } = action.payload;
-      /**
-       * TODO: Make API call to delete comments to db
-       */
+    //   state.commentData = res;
+    // },
+    // editComments: (state, action) => {
+    //   const { id, description } = action.payload;
+    //   /**
+    //    * TODO: Make API call to delete comments to db
+    //    */
 
-      const comments = JSON.parse(JSON.stringify(dummyData.comments));
-      console.log('Hi');
-      comments.forEach((comment) => {
-        if (comment.id === id) {
-          comment.description = description;
-        }
-      });
+    //   const comments = JSON.parse(JSON.stringify(dummyData.comments));
+    //   console.log('Hi');
+    //   comments.forEach((comment) => {
+    //     if (comment.id === id) {
+    //       comment.description = description;
+    //     }
+    //   });
 
-      state.commentData = comments;
-    },
+    //   state.commentData = comments;
+    // },
   },
 });
 
@@ -81,5 +82,18 @@ export const {
   deleteComments,
   editComments,
 } = commentSlice.actions;
+
+export const forumSlice = createSlice({
+  name: 'forum',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getAllForums.pending]: (state, action) => {
+      state.status = "loading";
+    }
+  },
+});
+
+export const {} = commentSlice.action;
 
 export default commentSlice.reducer;
