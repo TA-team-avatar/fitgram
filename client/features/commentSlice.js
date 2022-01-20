bbdf79f2999f3599fca01c7963f2385eb41c0af7
Mia Kang <miakangnyc@gmail.com>
2022-01-20 18:46:57 -0500

    updated dev so I have workoutSlice and cleaned up commentSlice

--- client/features/commentSlice.js
+++ client/features/commentSlice.js
@@ -3,77 +3,13 @@ import dummyData from '../constants/dummyData';
 
 const initialState = {
   commentData: [],
-  status: null;
+  status: null,
 };
 
 export const commentSlice = createSlice({
   name: 'comment',
   initialState,
-  reducers: {
-    // getForumComments: (state, action) => {
-    //   const forumId = action.payload.forumId;
-
-    //   /**
-    //    * TODO: Make API call to get forum comment information
-    //    */
-    //   let res = dummyData.comments.filter(
-    //     (comment) => comment.forum_id === forumId
-    //   );
-
-    //   if (res) {
-    //     res = JSON.parse(JSON.stringify(res));
-    //   }
-
-    //   state.commentData = res;
-    // },
-    // createComments: (state, action) => {
-    //   const { description, owner_user_id, forum_id } = action.payload;
-    //   /**
-    //    * TODO: Make API call to add comments to db
-    //    */
-
-    //   const comments = JSON.parse(JSON.stringify(dummyData.comments));
-    //   comments.push({
-    //     id: 4,
-    //     description,
-    //     owner_user_id,
-    //     forum_id,
-    //     date_created: '2022-01-15',
-    //     user_name: 'Han',
-    //   });
-    //   state.commentData = comments;
-    // },
-    // deleteComments: (state, action) => {
-    //   const { id } = action.payload;
-    //   /**
-    //    * TODO: Make API call to delete comments to db
-    //    */
-
-    //   let res = dummyData.comments.filter((comment) => comment.id !== id);
-
-    //   if (res) {
-    //     res = JSON.parse(JSON.stringify(res));
-    //   }
-
-    //   state.commentData = res;
-    // },
-    // editComments: (state, action) => {
-    //   const { id, description } = action.payload;
-    //   /**
-    //    * TODO: Make API call to delete comments to db
-    //    */
-
-    //   const comments = JSON.parse(JSON.stringify(dummyData.comments));
-    //   console.log('Hi');
-    //   comments.forEach((comment) => {
-    //     if (comment.id === id) {
-    //       comment.description = description;
-    //     }
-    //   });
-
-    //   state.commentData = comments;
-    // },
-  },
+  reducers: {},
 });
 
 export const {
@@ -89,11 +25,11 @@ export const forumSlice = createSlice({
   reducers: {},
   extraReducers: {
     [getAllForums.pending]: (state, action) => {
-      state.status = "loading";
-    }
+      state.status = 'loading';
+    },
   },
 });
 
-export const {} = commentSlice.action;
+export const {} = commentSlice.actions;
 
 export default commentSlice.reducer;
