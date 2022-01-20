c7276843739a483f55e700d7c07c66eae5ccc8a4
Mia Kang <miakangnyc@gmail.com>
2022-01-19 22:52:47 -0500

    completed commentSlice, still need to test

--- client/features/commentSlice.js
+++ client/features/commentSlice.js
@@ -1,17 +1,17 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from "axios";
+import axios from 'axios';
 import dummyData from '../constants/dummyData';
 
 const initialState = {
   commentData: {},
   commentList: [],
-  status: null;
+  status: null,
 };
 
 export const getForumComments = createAsyncThunk(
   'comments/getForumComments',
   async ({ forumId }) => {
-    const res = await axios.get(`/comments/${forumId}`)
+    const res = await axios.get(`/comments/${forumId}`);
     return res.data.comments;
   }
 );
@@ -19,7 +19,7 @@ export const getForumComments = createAsyncThunk(
 export const deleteComments = createAsyncThunk(
   'comments/deleteComments',
   async ({ owner_user_id, id }) => {
-    const res = await axios.delete('/comments', {owner_user_id, id});
+    const res = await axios.delete('/comments', { owner_user_id, id });
     return res.data.comments;
   }
 );
@@ -27,20 +27,22 @@ export const deleteComments = createAsyncThunk(
 export const editComments = createAsyncThunk(
   'comments/editComments',
   async ({ description, id }) => {
-    const res = await axios.put('/comments', {description, id})
+    const res = await axios.put('/comments', { description, id });
     return res.data.comment;
   }
-)
+);
 
 export const createComments = createAsyncThunk(
   'comments/createComments',
-  async ({owner_user_id, forum_id, description}) => {
+  async ({ owner_user_id, forum_id, description }) => {
     const res = await axios.post('/comments', {
-      owner_user_id, forum_id, description
+      owner_user_id,
+      forum_id,
+      description,
     });
     return res.data.comment;
   }
-) 
+);
 
 export const commentSlice = createSlice({
   name: 'comment',
@@ -52,43 +54,40 @@ export const commentSlice = createSlice({
     },
     [getForumComments.fulfilled]: (state, action) => {
       state.commentList = payload;
-      state.status = 'success'
+      state.status = 'success';
     },
     [getForumComments.rejected]: (state, action) => {
       state.status = 'failed';
     },
-
 
     [deleteComments.pending]: (state, action) => {
       state.status = 'loading';
     },
     [deleteComments.fulfilled]: (state, action) => {
       state.commentList = payload;
-      state.status = 'success'
+      state.status = 'success';
     },
     [deleteComments.rejected]: (state, action) => {
       state.status = 'failed';
     },
-
 
     [editComments.pending]: (state, action) => {
       state.status = 'loading';
     },
     [editComments.fulfilled]: (state, action) => {
       state.commentData = payload;
-      state.status = 'success'
+      state.status = 'success';
     },
     [editComments.rejected]: (state, action) => {
       state.status = 'failed';
     },
-
 
     [createComments.pending]: (state, action) => {
       state.status = 'loading';
     },
     [createComments.fulfilled]: (state, action) => {
       state.commentList = payload;
-      state.status = 'success'
+      state.status = 'success';
     },
     [createComments.rejected]: (state, action) => {
       state.status = 'failed';
