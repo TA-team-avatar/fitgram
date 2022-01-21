b52105267aa291b7748d42e392c23708f419d317
han900204 <54014070+han900204@users.noreply.github.com>
2022-01-20 17:12:38 -0800

    updates

--- client/containers/NavContainer.js
+++ client/containers/NavContainer.js
@@ -18,27 +18,31 @@ const NavContainer = () => {
   return (
     <div>
       <>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand">Fitness App</a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand">Fitness App</a>
             <div className="navbar-nav">
-              <ul class="nav nav-tabs">
-                <li class="nav-item">
+              <ul className="nav nav-tabs">
+                <li className="nav-item">
                   <a
-                    class="nav-link"
+                    className="nav-link"
                     data-bs-toggle="tab"
                     href="/profile/:userId"
                   >
                     Profile
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" data-bs-toggle="tab" href="/message/:id">
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    data-bs-toggle="tab"
+                    href="/message/:id"
+                  >
                     Messages
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" data-bs-toggle="tab" href="#">
+                <li className="nav-item">
+                  <a className="nav-link" data-bs-toggle="tab" href="#">
                     Logout(link)
                   </a>
                 </li>
